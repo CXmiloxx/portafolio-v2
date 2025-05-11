bfa4959b5411470f9cec02cc909e468eceafe09d
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-11 17:17:00 -0500

    ♻️ refactor(home): cambia nombre de la carpeta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Layout from './layout/Layout';
 import AboutPage from './pages/aboutPage/AboutPage';
-import HomePage from './pages/HomePage/HomePage';
+import HomePage from './pages/homePages/HomePage';
 import ProyectsPage from './pages/proyectsPage/proyectsPage';
 
 export default function App() {
