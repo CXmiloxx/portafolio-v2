f4b922d3de59faa3492a17deaad1baaefb4f842e
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-11 17:23:06 -0500

    ♻️ refactor(App): elimina componente ProyectsPage y ajusta la estructura de renderizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Layout from './layout/Layout';
 import AboutPage from './pages/aboutPage/AboutPage';
 import HomePage from './pages/homePages/HomePage';
-import ProyectsPage from './pages/proyectsPage/proyectsPage';
 
 export default function App() {
   return (
     <Layout>
       <HomePage />
-      <ProyectsPage/>
       <AboutPage/>
     </Layout>
   );
