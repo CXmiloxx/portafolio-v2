917fb6c5c084036c790fdbb44d3a0c5eae095ab3
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-11 17:06:21 -0500

    ✨ feat(main): envuelve App en LanguageProvider y ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import { LanguageProvider } from './context/LanguageContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </LanguageProvider>
   </StrictMode>,
 )
