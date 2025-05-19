7694c940d98371ece21236ad9c2479dc84b94589
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-18 20:51:38 -0500

    ✨ feat(NavItem): reemplaza anchor por button y añade funcionalidad de scroll
    Reemplaza el elemento `<a>` por un `<button>` para manejar mejor los eventos de clic. Añade la función `handleClick` para ejecutar acciones de scroll específicas según la propiedad `id` y cerrar el menú de navegación si aplica. Mejora la interacción y la experiencia de navegación del usuario.

--- src/components/NavBar/NavItem.jsx
+++ src/components/NavBar/NavItem.jsx
@@ -1,16 +1,34 @@
-export default function NavItem({ label, active = false }) {
+export default function NavItem({ label, id, active = false }) {
+  const scrollFunctions = {
+    home: window.scrollToHome,
+    about: window.scrollToAbout,
+    projects: window.scrollToProjects,
+    skills: window.scrollToSkills,
+    contact: window.scrollToContact,
+  };
+
+  const handleClick = () => {
+    if (scrollFunctions[id]) {
+      scrollFunctions[id]();
+    }
+
+    if (window.closeNavMenu) {
+      window.closeNavMenu();
+    }
+  };
+
   return (
     <li>
-      <a
-        href="#"
+      <button
+        onClick={handleClick}
         className={`block px-4 py-2 rounded-md transition-all ${
           active
-            ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-blue-500'
-            : 'text-gray-700 hover:bg-gray-100 md:hover:text-blue-600 dark:text-white dark:hover:text-blue-400'
+            ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-blue-500"
+            : "text-gray-700 hover:bg-gray-100 md:hover:text-blue-600 dark:text-white dark:hover:text-blue-400"
         }`}
       >
         {label}
-      </a>
+      </button>
     </li>
   );
 }
