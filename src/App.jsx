c8138afd3b780d82a0cfdee10ac42f7b66e3129e
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-18 20:58:16 -0500

    ♻️ refactor: elimina componente AboutPage sin usar de App.jsx
    Elimina la importación y el renderizado del componente AboutPage en App.jsx, ya que ya no es necesario. Esta limpieza mejora el mantenimiento del código.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Layout from './layout/Layout';
-import AboutPage from './pages/aboutPage/AboutPage';
 import HomePage from './pages/homePages/HomePage';
 
 export default function App() {
   return (
     <Layout>
       <HomePage />
-      <AboutPage/>
     </Layout>
   );
 }
