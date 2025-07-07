3d940f3f78f94ae26863bb12cccb945519a80e97
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-07-06 20:22:31 -0500

    ✨ feat(Layout): mejora el diseño del componente Layout
    - Actualiza el contenedor principal para incluir clases de fondo que permiten un cambio de color suave entre modos claro y oscuro.
    - Elimina un espacio innecesario en la clase de padding del contenedor de hijos, optimizando el estilo general.

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,9 +2,9 @@ import NavBar from "../components/NavBar/NavBar";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="bg-white dark:bg-black transition-colors duration-500">
       <NavBar />
-      <div className="pt-18 ">
+      <div className="pt-18">
         {children}
       </div>
     </div>
